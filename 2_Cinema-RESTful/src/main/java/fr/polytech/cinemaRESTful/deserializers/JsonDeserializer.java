9715cf6ae61385b2db20bd76476e20b40ee7026c
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-12 16:03:30 +0200

    Add JSON serializer and deserializer

---
+++ 2_Cinema-RESTful/src/main/java/fr/polytech/cinemaRESTful/deserializers/JsonDeserializer.java
@@ -0,0 +1,14 @@
+package fr.polytech.cinemaRESTful.deserializers;
+
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+
+public class JsonDeserializer implements Deserializer<String> {
+
+    public static final Jsonb JSON_BUILDER = JsonbBuilder.create();
+
+    @Override
+    public <O> O from(String in, Class<O> clazz) {
+        return JSON_BUILDER.fromJson(in, clazz);
+    }
+}
