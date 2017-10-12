8e704b819134052550627bc3e40df560ebf2d20c
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-12 11:19:13 +0200

    Rename entry of the application

--- 2_Cinema-RESTful/src/main/java/fr/polytech/cinemaRESTful/CinemaResTfulApplication.java
+++ 2_Cinema-RESTful/src/main/java/fr/polytech/cinemaRESTful/CinemaResTfulApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CinemaResTfulApplication {
+public class CinemaRestfulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CinemaResTfulApplication.class, args);
+		SpringApplication.run(CinemaRestfulApplication.class, args);
 	}
 }
