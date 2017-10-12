59e39e4618641542267bdbdb78333fb230ebddcb
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-12 10:05:15 +0200

    Add initial Cinema RESTful project

---
+++ 2_Cinema-RESTful/src/main/java/fr/polytech/cinemaRESTful/CinemaResTfulApplication.java
@@ -0,0 +1,12 @@
+package fr.polytech.cinemaRESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaResTfulApplication.class, args);
+	}
+}
