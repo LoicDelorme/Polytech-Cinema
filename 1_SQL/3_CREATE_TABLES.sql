2a6faf402365fe73472e25316ce91d3972d4ccf2
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-12 09:31:54 +0200

    Add SQL scripts

---
+++ 1_SQL/3_CREATE_TABLES.sql
@@ -0,0 +1,51 @@
+USE TP_CinemaRESTful;
+
+/*
+  DROP TABLE IF EXISTS characters;
+  DROP TABLE IF EXISTS movies;
+  DROP TABLE IF EXISTS categories;
+  DROP TABLE IF EXISTS directors;
+  DROP TABLE IF EXISTS actors;
+*/
+
+CREATE TABLE actors (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  lastname VARCHAR(100) NOT NULL,
+  firstname VARCHAR(100),
+  birth_date DATE,
+  date_of_death DATE
+);
+
+CREATE TABLE directors (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  lastname VARCHAR(100) NOT NULL,
+  firstname VARCHAR(100)
+);
+
+CREATE TABLE categories (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  restricted_label VARCHAR(2) NOT NULL UNIQUE,
+  full_label VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE movies (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title VARCHAR(20) NOT NULL UNIQUE,
+  duration INT NOT NULL,
+  released_date DATE NOT NULL,
+  budget INT NOT NULL,
+  amont_of_revenue INT NOT NULL,
+  id_director INT NOT NULL,
+  id_category INT NOT NULL,
+  FOREIGN KEY (id_director) REFERENCES directors (id),
+  FOREIGN KEY (id_category) REFERENCES categories (id)
+);
+
+CREATE TABLE characters (
+  id_movie INT NOT NULL,
+  id_actor INT NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id_movie, id_actor),
+  FOREIGN KEY (id_movie) REFERENCES movies (id),
+  FOREIGN KEY (id_actor) REFERENCES actors (id)
+);
---
+++ 1_SQL/4_INSERT_DATA.sql
@@ -0,0 +1,39 @@
+USE TP_CinemaRESTful;
+
+INSERT INTO actors (lastname, firstname, birth_date, date_of_death) VALUES
+	('Reno', 'Jean', '1948-07-30', NULL),
+	('Portman', 'Natalie', '1981-06-09', NULL),
+	('Dujardin', 'Jean', '1972-06-19', NULL),
+	('Bourvil', '', '1917-07-27', '1970-09-23'),
+	('De Funes', 'Louis', '1914-07-31', '1983-01-27'),
+	('Anglade', 'Jean-Hugues', '1955-07-29', NULL),
+	('Lambert', 'Christophe', '1957-03-29', NULL);
+
+INSERT INTO directors (lastname, firstname) VALUES
+	('Oury', 'Gérard'),
+	('Chabrol', 'Claude'),
+	('Besson', 'Luc'),
+	('Besnard', 'Eric');
+
+INSERT INTO categories (restricted_label, full_label) VALUES
+	('AC', 'Action'),
+	('CO', 'Comédie'),
+	('PO', 'Policier'),
+	('WE', 'Western');
+
+INSERT INTO movies (title, duration, released_date, budget, amont_of_revenue, id_director, id_category) VALUES
+	('Léon', 110, '1994-04-14', 17531000, 69250000, 3, 3),
+	('Cash', 100, '2008-04-23', 18340000, 60340000, 4, 3),
+	('La grande vadrouille', 132, '1966-12-01', 7227000, 51258000, 2, 1),
+	('Subway', 104, '1985-04-10', 10567000, 70500000, 3, 3);
+
+INSERT INTO characters (id_movie, id_actor, name) VALUES
+	(1, 1, 'Léon'),
+	(1, 5, 'Mathilda'),
+	(2, 1, 'Maxime Dubreuil'),
+	(2, 5, 'Cash'),
+	(3, 4, 'Augustin Bouvet'),
+	(3, 5, 'Stanislas Lefort'),
+	(4, 1, 'Le Batteur'),
+	(4, 2, 'Le Roller'),
+	(4, 3, 'Fred');
