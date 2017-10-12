a17ef3840b9138d4bb9cd350cd0454648082d48d
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-12 16:04:12 +0200

    Declare all beans of the application

--- 2_Cinema-RESTful/src/main/java/fr/polytech/cinemaRESTful/CinemaResTfulApplication.java
+++ 2_Cinema-RESTful/src/main/java/fr/polytech/cinemaRESTful/CinemaResTfulApplication.java
@@ -1,12 +1,45 @@
 package fr.polytech.cinemaRESTful;
 
+import fr.polytech.cinemaRESTful.persistence.DatabaseManager;
+import fr.polytech.cinemaRESTful.services.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CinemaRestfulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaRestfulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaRestfulApplication.class, args);
+    }
+
+    @Bean
+    public DatabaseManager databaseManager() {
+        return new DatabaseManager();
+    }
+
+    @Bean
+    public ActorDaoServices actorDaoServices() {
+        return new ActorDaoServices();
+    }
+
+    @Bean
+    public CategoryDaoServices categoryDaoServices() {
+        return new CategoryDaoServices();
+    }
+
+    @Bean
+    public CharacterDaoServices characterDaoServices() {
+        return new CharacterDaoServices();
+    }
+
+    @Bean
+    public DirectorDaoServices directorDaoServices() {
+        return new DirectorDaoServices();
+    }
+
+    @Bean
+    public MovieDaoServices movieDaoServices() {
+        return new MovieDaoServices();
+    }
 }
