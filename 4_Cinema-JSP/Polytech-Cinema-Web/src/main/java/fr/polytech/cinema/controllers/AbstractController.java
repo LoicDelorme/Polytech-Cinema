2ae3cc0346cae9e9f08e036725a797450c7ba124
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-11-03 19:41:36 +0100

    Fix UTF-8 issue + error redirection

--- 4_Cinema-JSP/Polytech-Cinema-Web/src/main/java/fr/polytech/cinema/controllers/AbstractController.java
+++ 4_Cinema-JSP/Polytech-Cinema-Web/src/main/java/fr/polytech/cinema/controllers/AbstractController.java
@@ -10,7 +10,7 @@ import fr.polytech.cinema.restful.RestfulRequester;
 public class AbstractController {
 
 	public static final Logger LOGGER = Logger.getLogger(AbstractController.class.getSimpleName());
-
+	
 	public static final Deserializer<String> DESERIALIZER = new JsonDeserializer();
 
 	public static final RestfulRequester RESTFUL_REQUESTER = new JsonRestfulRequester("http://localhost:8090/api/");
